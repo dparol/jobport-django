5ef904027b36a71b5f52d63344b7bba302843d76
dparol <dikshithparol123@gmail.com>
2023-01-24 09:51:02 +0530

    postgres add

--- Jobport/settings.py
+++ Jobport/settings.py
@@ -130,10 +130,14 @@ WSGI_APPLICATION = 'Jobport.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'jobportal',
+       'USER': 'postgres',
+       'PASSWORD': 'qwedfgbnm',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
